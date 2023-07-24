f2b0b21573a5ba86ba1da36507bfb99bb54ca5c2
Pinzack <cahitblk@hotmail.com>
2023-07-24 23:50:48 +0300

    10. Ders Sonu

--- src/test/java/tests/day10_waits_cookies_webTables/C01_ExplicitlyWait.java
+++ src/test/java/tests/day10_waits_cookies_webTables/C01_ExplicitlyWait.java
@@ -1,4 +1,50 @@
 package tests.day10_waits_cookies_webTables;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C01_ExplicitlyWait {
+
+
+    //2. Iki tane metod olusturun : implicitWait() , explicitWait()
+    //
+    //   Iki metod icin de asagidaki adimlari test edin.
+    //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+    //4. Remove butonuna basin.
+    //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+    //6. Add buttonuna basin
+    //7. It’s back mesajinin gorundugunu test edin
+
+
+
+
+    @Test
+
+    public  void implicitlyWaitTest(){
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        //4. Remove butonuna basin.
+
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+
+
+
+
+
+
+    }
 }
